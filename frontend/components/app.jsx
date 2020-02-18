cd61543862160217ba6c53a7c667ed1963af26de
derek schear <dschear94@gmail.com>
2020-02-18 14:34:44 -0500

    updates to config

---
+++ frontend/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+const App = () => {
+    return (
+    <div>
+        {/* <Route path="/"><NavbarContainer /></Route> */}
+        hi
+    </div>
+)};
+
+export default App;
---
+++ frontend/entry.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser },
+                // likes: window.currentUserLikes,
+                // follows: window.currentUserFollows,
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+        // delete window.currentUserLikes;
+        // delete window.currentUserFollows;
+    } else {
+        store = configureStore();
+    }
+
+    const root = document.getElementById("yo");
+    ReactDOM.render(<Root store={store} />, root);
+});
